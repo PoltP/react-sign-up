f14a50b887e083dc7df175e8caddc2fd8c64cee7
Pavel Poltavets <pavel.a.poltavets@gmail.com>
2021-06-04 18:19:17 +0300

    Add supported breakpoints for media queries

---
+++ src/components/media-queries.ts
@@ -0,0 +1,9 @@
+export const mediaQueries = {
+    mobile: `(max-width: 375px)`,
+    mobile414: `(max-width: 414px)`,
+    mobile640: `(max-width: 640px)`,
+    tablet768: `(max-width: 768px)`,
+    tablet: `(max-width: 1024px)`,
+    laptop1280: `(max-width: 1280px)`,
+    laptop1440: `(max-width: 1440px)`
+}
